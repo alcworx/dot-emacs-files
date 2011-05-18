8282841d8cbe5fb6fc78843196c5ef2a4466075e
Pavel Ryumin <alcworx@gmail.com>
2011-05-18 12:39:06 -0700

    haskell mode dirty fix

--- configs/pavel-haskell.el
+++ configs/pavel-haskell.el
@@ -1,3 +1,20 @@
+;; haskell mode configuration
+(setq auto-mode-alist
+      (append auto-mode-alist
+              '(("\\.[hg]s$"  . haskell-mode)
+                ("\\.hic?$"     . haskell-mode)
+                ("\\.hsc$"     . haskell-mode)
+  ("\\.chs$"    . haskell-mode)
+                ("\\.l[hg]s$" . literate-haskell-mode))))
+(autoload 'haskell-mode "haskell-mode"
+   "Major mode for editing Haskell scripts." t)
+(autoload 'literate-haskell-mode "haskell-mode"
+   "Major mode for editing literate Haskell scripts." t)
+(require 'haskell-mode)
+(require 'inf-haskell) 
+(custom-set-variables
+;; snip
+   '(haskell-program-name "ghci")) 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
