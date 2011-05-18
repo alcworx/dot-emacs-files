b01466e1196a7cd985221a55db2f0c9584d90377
Pavel Ryumin <alcworx@gmail.com>
2011-05-18 12:42:57 -0700

    strange fix for w3m browser, still doesn't work on emacs24

--- configs/pavel-w3m.el
+++ configs/pavel-w3m.el
@@ -3,5 +3,6 @@
  ;; optional keyboard short-cut
 ;;  (global-set-key "\C-xm" 'browse-url-at-point)
 (setq w3m-use-cookies t)
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/w3m")
 (require 'w3m-load)
 (provide 'pavel-w3m)
