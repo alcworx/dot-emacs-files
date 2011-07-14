d850b090a4865b39c1339aafcbfdd8c6f1e57d2d
Pavel Ryumin <alcworx@gmail.com>
2011-07-14 14:10:35 +0400

    org files updated

--- configs/pavel-org.el
+++ configs/pavel-org.el
@@ -33,7 +33,37 @@
       '((sequence "TODO(t)" "WAITING(w@/!)" "STARTED(s!)" "NEXT(n!)" "MAYBE(m!)" "|"  "DEFERRED(f)" "DONE(d!)" "CANCELLED(c@)")))
 (setq org-use-fast-todo-selection t)
 (setq org-default-notes-file (concat "~/Dropbox/org" "/notes.org"))
+
+
 (define-key global-map "\C-cc" 'org-capture)
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, and org-protocol
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/Dropbox/org/refile.org")
+               "** TODO %?\n%U\n  %i" :clock-in t :clock-resume t)
+              ("l" "todo with link" entry (file "~/Dropbox/org/refile.org")
+               "** TODO %?\n%U\n%a\n  %i" :clock-in t :clock-resume t)
+              ("n" "note" entry (file "~/Dropbox/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n  %i" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/Dropbox/org/diary.org")
+               "* %?\n%U\n  %i" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/Dropbox/org/refile.org")
+               "* TODO Review %c\n%U\n  %i" :immediate-finish t)
+              ("p" "Phone call" entry (file "~/Dropbox/org/refile.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/Dropbox/org/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %t .+1d/3d\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n  %i"))))
+
+;; Remove empty LOGBOOK drawers on clock out
+(defun bh/remove-empty-drawer-on-clock-out ()
+  (interactive)
+  (save-excursion
+    (beginning-of-line 0)
+    (org-remove-empty-drawer-at "LOGBOOK" (point))))
+
+(add-hook 'org-clock-out-hook 'bh/remove-empty-drawer-on-clock-out 'append)
+
+
 ;;(setq org-treat-S-cursor-todo-selection-as-state-change nil)
 
 (eval-after-load "org-agenda"
