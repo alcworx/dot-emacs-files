7652b7d1dab5c60069f32cf868960ea27ba6c274
Pavel Ryumin <alcworx@gmail.com>
2012-04-09 02:49:57 +0400

    various necessary filetypes added

--- configs/pavel-btools.el
+++ configs/pavel-btools.el
@@ -1,7 +1,12 @@
  (setq auto-mode-alist
       (cons '("SConstruct" . python-mode) auto-mode-alist))
  (setq auto-mode-alist
-      (cons '("SConscript" . python-mode) auto-mode-alist))
+       (cons '("SConscript" . python-mode) auto-mode-alist))
+ (setq auto-mode-alist
+       (cons '(".conkerorrc" . javascript-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.cu$" . c++-mode))
  (setq auto-mode-alist
        (cons '("wscript" . python-mode) auto-mode-alist))
+ (setq auto-mode-alist
+       (cons '("PKGBUILD" . shell-script-mode) auto-mode-alist))
 (provide 'pavel-btools)
